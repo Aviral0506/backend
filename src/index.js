fa316e8cef8e3d608be79500d14bd4fb0dc0e177
Aviral <aviralchaurasia175@gmail.com>
2025-09-13 01:33:55 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -9,21 +9,14 @@ dotenv.config({
 
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
-
-
-
-
-
-
-
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is running in the ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed" , err)
+})
 
 
 
